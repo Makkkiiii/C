a09e64fd9afa273060cd231e49bca617e30162eb
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-05-16 12:36:18 +0545

    NO modified Pattern to print opposite

--- String10b.c
+++ String10b.c
@@ -3,21 +3,17 @@
 
 int main()
 {
-    char str[] = "Denish";
+    char str[] = "DENISH";
     int length = strlen(str);
     int i, j;
 
     for (i = 0; i < length; i++)
     {
-        for (j = 0; j <= i; j++)
-        {
-            printf("\n");
-        }
-        for (j = 0; j < length - i; j++)
+        for (j = i; j < length; j++)
         {
             printf("%c", str[j]);
         }
-        // printf("\n");
+        printf("\n");
     }
 
     return 0;
