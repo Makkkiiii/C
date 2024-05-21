581934cc9493dc392b08b6367c7dafdc5bd791b2
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-05-21 13:42:44 +0545

    modified

--- tester.c
+++ tester.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+
 int main()
 {
-    int x = printf("Hello");
-    printf("%d", x);
+    int i, j, n = 3; // n is the number of rows in the upper half of the pattern
+
+    // Print the upper half of the pattern
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - i; j++)
+            printf("  ");
+        for (j = 0; j < 2 * i + 1; j++)
+            printf("* ");
+        printf("\n");
+    }
+
+    // Print the lower half of the pattern
+    for (i = n - 2; i >= 0; i--)
+    {
+        for (j = 0; j < n - i; j++)
+            printf("  ");
+        for (j = 0; j < 2 * i + 1; j++)
+            printf("* ");
+        printf("\n");
+    }
+
     return 0;
 }
