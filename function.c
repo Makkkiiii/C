0f32dbcc69a1d97f38e3adaaccc94e84873e0a5e
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-05-23 12:54:19 +0545

    function with argument and return type

--- function.c
+++ function.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+// Function with arguments and return value
 int addition(int a, int b)
 {
     int sum;
     sum = a + b;
+    return sum;
 }
 int main()
 {
-
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+    int z = addition(x, y);
+    printf("The sum of %d and %d is %d\n", x, y, z);
     return 0;
 }
