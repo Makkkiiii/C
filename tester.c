d92ae12e3b38f6133710e643e89bdda65437a1cc
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-05-23 12:40:40 +0545

    modified

--- tester.c
+++ tester.c
@@ -1,41 +1,17 @@
 #include <stdio.h>
-#include <string.h>
-
-typedef struct
-{
-    char service[50];
-    char password[50];
-} PasswordEntry;
-
-PasswordEntry passwordDatabase[100];
-int dbSize = 0;
-
-void addPassword(char *service, char *password)
-{
-    strcpy(passwordDatabase[dbSize].service, service);
-    strcpy(passwordDatabase[dbSize].password, password);
-    dbSize++;
-}
-
-char *getPassword(char *service)
-{
-    for (int i = 0; i < dbSize; i++)
-    {
-        if (strcmp(passwordDatabase[i].service, service) == 0)
-        {
-            return passwordDatabase[i].password;
-        }
-    }
-    return NULL;
-}
 
 int main()
 {
-    addPassword("email", "password123");
-    addPassword("bank", "123456");
+    int i, j;
 
-    printf("Email password: %s\n", getPassword("email"));
-    printf("Bank password: %s\n", getPassword("bank"));
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", i * j);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
