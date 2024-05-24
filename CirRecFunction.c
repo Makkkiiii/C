d524fe1e50abe998f61cb195b3f10741cc435d73
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-05-24 10:44:29 +0545

    Program to calculate Area of circle and rectangle

---
+++ CirRecFunction.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define pi 3.14
+float areaOfCircle()
+{
+    float r, area;
+    printf("Enter the radius of circle: ");
+    scanf("%f", &r);
+    area = pi * r * r;
+    return area;
+}
+float areaofRectangle()
+{
+    float l, b, area;
+    printf("Enter the length and breadth of rectangle: ");
+    scanf("%f %f", &l, &b);
+    area = l * b;
+    return area;
+}
+
+int main()
+{
+    int choice;
+    float area;
+    printf("Enter 1 for area of circle and 2 for area of rectangle: ");
+    scanf("%d", &choice);
+    if (choice == 1)
+    {
+        area = areaOfCircle();
+        printf("The area of circle is: %.2f", area);
+    }
+    else if (choice == 2)
+    {
+        area = areaofRectangle();
+        printf("The area of rectangle is: %.2f", area);
+    }
+    else
+    {
+        printf("Invalid choice");
+    }
+    return 0;
+}
