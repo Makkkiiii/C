307125144843beddb5b4ac5dc9646a5ef910e519
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-05-30 13:01:45 +0545

    modified
    modified

--- primerecursion.c
+++ primerecursion.c
@@ -2,13 +2,12 @@
 
 int isPrime(int n, int i)
 {
-    if (n < 2)
-        return 0;
     if (i == 1)
         return 1;
-    if (n % i == 0)
+    else if (n % i == 0)
         return 0;
-    return isPrime(n, i - 1);
+    else
+        return isPrime(n, i - 1);
 }
 
 int main()
@@ -18,11 +17,11 @@ int main()
     scanf("%d", &num);
     if (isPrime(num, num / 2))
     {
-        printf("%d is a prime number", num);
+        printf("Prime number", num);
     }
     else
     {
-        printf("%d is not a prime number", num);
+        printf("Not prime number", num);
     }
     return 0;
 }
