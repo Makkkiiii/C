2af882abd8fd135973465cf56f93d35bc90eb37a
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-05 09:10:35 +0545

    modified

--- nullpointer.c
+++ nullpointer.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    int a[] = {5, 2, 1, 4, 3};
-    int *p;
-    p = &a;
-    for (int i = 0; i < 5; i++)
-    {
-        printf("\n%d", *(p + i));
-    }
-
+    printf("Hello, World!\n");
     return 0;
 }
